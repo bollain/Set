79ced805b61f7d8c27a30f4758d98b7c5782c55d
Armando Dorantes Bollain <bollain@gmail.com>
2018-02-09 15:18:12 -0800

    Initial Commit

---
+++ Set/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Set
+//
+//  Created by Armando Dorantes Bollain y Goytia on 2018-02-09.
+//  Copyright © 2018 Armando Dorantes Bollain y Goytia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
